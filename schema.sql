2686f11c8f21eba5bcd7c2f1a8bd238f875032d5
Lesley Moore <ldmoore15@gmail.com>
2021-06-21 12:16:06 -0400

    modified:   schema.sql

--- schema.sql
+++ schema.sql
@@ -5,10 +5,24 @@ CREATE DATABASE employeedb;
 USE employeedb;
 
 CREATE TABLE employee (
-    id INT AUTOINCREMENT, 
+    id INT AUTO_INCREMENT, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT FK,
     manager_id INT FK,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
     PRIMARY KEY (id)
 )
